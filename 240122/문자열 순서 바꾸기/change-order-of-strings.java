a0ce35aaad0d6c5e88fa756ad24471001e0ee6c9
Yunjae Lee <135799134+itsjaeya@users.noreply.github.com>
2024-01-22 10:14:55 +0900

    Title: 문자열 순서 바꾸기, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240122/문자열 순서 바꾸기/change-order-of-strings.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String t = sc.next();
+        String temp = "";
+        
+        temp.equals(s);
+        s.equals(t);
+        t.equals(temp);
+
+        System.out.println(t);
+        System.out.println(s);
+    }
+}
